aacd42a7db2b5850ff8aa3ac147e2db20760fe8c
Cristian Granada <cristiansordo11@gmail.com>
2020-11-01 14:25:45 -0500

    crear ejercicio 2

--- secuenciales/ejercicio1/ejercicio1.js
+++ secuenciales/ejercicio1/ejercicio1.js
@@ -1,1 +1,26 @@
-ejercicio1.js
+//  1.	Un alumno desea saber cuál será su calificación final en la materia de lógica y programación. 
+//  Dicha calificación se compone de los siguientes porcentajes.
+//  55% del promedio de sus tres calificaciones parciales.
+//  30% de la calificación del examen final.
+//  15% de la calificación de un trabajo final.
+
+let nota1;
+let nota2;
+let nota3;
+let media;
+let examen;
+let trabajo;
+let resultado=0;
+
+nota1 = Number(prompt('Ingrese la primera nota de parcial: '));
+nota2 = Number(prompt('Ingrese la segunda nota de parcial: '));
+nota3 = Number(prompt('Ingrese la tercera nota de parcial: '));
+exmaen = Number(prompt('Ingrese la nota del examen final: '));
+trabajo = Number(prompt('Ingrese la nota del trabajo final: '));
+
+media=(nota1+nota2+nota3)/3;
+resultado = media*.55+examen*.3+trabajo*.15;
+
+console.log('El alumno, su calificacion final: '+ resultado );
+alert('El alumno, su calificacion final: '+ resultado );
+document.write('El alumno, su calificacion final: '+ resultado );
